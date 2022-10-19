ea055548e0027bae35812073bdf7d6c7e8b42ab1
Bmonky13 <99373064+Bmonky13@users.noreply.github.com>
2022-10-19 10:27:16 -0600

    Add project files.

---
+++ E-Tutoring-Greame/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Tutoring_Greame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
