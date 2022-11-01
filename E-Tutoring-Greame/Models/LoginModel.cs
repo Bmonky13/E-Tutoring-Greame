e8730a9fa002c10107c8f484f6489f0dfd98f2ea
ashesndust314 <ashleymkang@gmail.com>
2022-11-01 13:21:34 -0600

    added forms

--- E-Tutoring-Greame/Models/LoginModel.cs
+++ E-Tutoring-Greame/Models/LoginModel.cs
@@ -4,5 +4,10 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Input1 { get; set; }
+        public string Input2 { get; set; }
+        public string Input3 { get; set; }
+        public string Input4 { get; set; }
+        public string Input5 { get; set; }
     }
 }
---
+++ E-Tutoring-Greame/Services/NumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+
+namespace E_Tutoring_Greame.Services
+{
+    public class NumberGenerator
+    {
+        string[] operators = { " + ", " - ", " * ", " / " };
+        public string generateExpression()
+        {
+
+            return "1";
+        }
+
+        public void getAnswers()
+        {
+            
+        }
+    }
+}
